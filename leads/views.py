5d5e5170e610cd1c778d5a7cff5cbc9dcd39a733
Francisco De La Cruz <helloquico@gmail.com>
2021-07-27 16:57:21 -0400

    Templates

--- leads/views.py
+++ leads/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, "leads/home_page.html")
