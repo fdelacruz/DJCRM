a75e1248cf187827bb9b6db580e47f7f0b8f8237
Francisco De La Cruz <helloquico@gmail.com>
2021-07-30 21:40:30 -0400

    Authentication

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = "static_root"
+STATIC_ROOT = 'static_root'
 
 AUTH_USER_MODEL = 'leads.User'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = '/leads'
--- leads/views.py
+++ leads/views.py
@@ -1,30 +1,35 @@
 from django.core.mail import send_mail
-from .forms import LeadModelForm
-from .models import Lead
 from django.shortcuts import reverse
-from django.views.generic import (
-    TemplateView, ListView, DetailView,
-    CreateView, UpdateView, DeleteView
-)
+from django.views import generic
+from .models import Lead
+from .forms import LeadModelForm, CustomUserCreationForm
 
 
-class LandingPageView(TemplateView):
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
+
+
+class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
 
 
-class LeadListView(ListView):
+class LeadListView(generic.ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"  # default configuration: object_list
 
 
-class LeadDetailView(DetailView):
+class LeadDetailView(generic.DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -38,10 +43,10 @@ class LeadCreateView(CreateView):
             from_email="test@they.com",
             recipient_list=["test@me.com"]
         )
-        return super(LeadCreateView, self).form_valid(form)
+        return super(generic.LeadCreateView, self).form_valid(form)
 
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -50,7 +55,7 @@ class LeadUpdateView(UpdateView):
         return reverse('leads:lead-list')
 
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
